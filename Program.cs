fbfe28faf3ba40b50fcd6ea94827528740e163dc
agiknightt <70384293+agiknightt@users.noreply.github.com>
2020-09-05 12:47:03 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {1,2,3,4,5,6,7,8 };
+
+            Shuffle(array);
+
+            foreach (int value in array)
+            {
+                Console.WriteLine(value);
+            }
+            Console.ReadKey();
+        }
+        static void Shuffle(int[] array)
+        {
+            Random rand = new Random();
+            int maxValue = array.Length;
+            
+            for (int i = 0; i < maxValue - 1; i++)
+            {
+                int random = rand.Next(i,maxValue);
+                int tempNumber = array[random];
+                array[random] = array[i];
+                array[i] = tempNumber;
+            }
+        }
+    }
+}
